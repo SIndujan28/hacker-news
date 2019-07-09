1782c933e8135d5436c029e5f1460b91023baaec
SIndujan28 <30890973+SIndujan28@users.noreply.github.com>
2019-07-09 15:29:47 +0530

    renders basic list from a object

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [ { title: 'React',
+                 url: 'https://facebook.github.io/react/', 
+                 author: 'Jordan Walke', 
+                 num_comments: 3, 
+                 points: 4, 
+                 objectID: 0, },
+                 { title: 'Graphql',
+                 url: 'https://graphql.org/', 
+                 author: 'Facebook', 
+                 num_comments: 3, 
+                 points: 3, 
+                 objectID: 1, },
+                 { title: 'React-redux',
+                 url: 'https://redux.js.org/', 
+                 author: 'Dan Abramov and Andrew Clark', 
+                 num_comments: 3, 
+                 points: 3, 
+                 objectID: 2, },
+                 
+                 ];
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      list:list,
+    }
+  }
+
+  render() {
+    return (
+      <div>
+       {this.state.list.map(item=>
+         <div key={item.objectID}>
+           <span>
+             <a href={item.url}>{item.title}</a>
+           </span>
+           <span>{item.author}</span>
+           <span>{item.num_comments}</span>
+           <span>{item.points}</span>
+          </div>
+       )}
+      </div>
+    )
+  }
 }
 
 export default App;
